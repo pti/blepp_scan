b1307a5e6b3da5a2d37d14460df1dcc8ee704663
Pekko Tiitto <tiittop@gmail.com>
2020-02-04 21:30:15 +0200

    Modified build script to grant necessary permissions.

--- build.sh
+++ build.sh
@@ -8,3 +8,7 @@ cd build
 cmake ..
 make
 cd -
+
+# Grant permissions required for BLE scanning to the binary file:
+BIN_FILE=`realpath ./build/blepp_scan`
+sudo setcap cap_net_raw+eip $(eval readlink -f "$BIN_FILE")
